cffa0d6ecebc58944d46712961a04dc0b729aae5
Petros Anagnou <petros.anagnoy@gmail.com>
2025-01-19 16:24:13 +0000

    Set brightness before filling in startup animation

--- led_strip/operations.py
+++ led_strip/operations.py
@@ -6,8 +6,8 @@ logger = logging.getLogger(__name__)
 
 def fill(led_strip: LedStrip, color: tuple[int, int, int], brightness: float) -> None:
     logger.info(f"Filling strip with color {color} and brightness {brightness}")
-    led_strip.strip.fill(color)
     led_strip.strip.brightness = brightness
+    led_strip.strip.fill(color)
 
 
 def clear(led_strip: LedStrip) -> None:
